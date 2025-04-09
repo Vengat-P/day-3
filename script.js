a9a16e2904e5d6c620ef25d474a339e6f4742af1
Vengat-P <cronyvengat33@gmail.com>
2025-04-09 15:51:08 +0530

    completed

--- script.js
+++ script.js
@@ -79,15 +79,15 @@ async function foo() {
     for (let i = 0; i < data.length; i++) {
       const div = document.createElement("div");
       div.innerHTML = `
-        <div class="container border w-[400px] h-[400px]flex flex-col  justify-center items-center py-2 mx-auto bg-red-200" >
-        <header class="cardheader flex items-center justify-center px-2 py-2 mt-2">
-            <h1 class="name text-xl font-bold uppercase text-black  ">${data[i].name.common}</h1>
+        <div class="container border w-auto h-auto flex flex-col justify-center rounded-md items-center py-0 mx-auto mt-10 bg-white" >
+        <header class="cardheader flex items-center justify-center w-full px-2 py-2 mb-4 mt-0 rounded-t-md bg-purple-500 ">
+            <h1 class="name text-2xl font-bold uppercase text-black  ">${data[i].name.common}</h1>
         </header>
-        <div class="cardimage px-2 py-2 mt-2 mb-2 flex justify-center w-auto h-24 items-center"><img src="${data[i].flags.png}" alt="1" class="w-auto h-24"></div>
-        <div class="details flex flex-col">
-            <h2 class="region text-xl font-semibold capitalize flex justify-center text-center p-4 mx-2 ">${data[i].region}</h2>
-            <h2 class="subregion text-xl font-semibold  capitalize flex justify-center text-center p-4 mx-2">${data[i].subregion}</h2>
-            <h2 class="population text-xl font-semibold  capitalize flex justify-center text-center p-4 mx-2">${data[i].population}</h2>
+        <div class="cardimage px-2 py-2 mt-2 mb-2 flex justify-center w-auto h-24 items-center"><img src="${data[i].flags.png}" alt="1" class="w-auto h-24 shadow-md shadow-black"></div>
+        <div class="details flex flex-col w-full mt-4 mb-0 rounded-b-md bg-gray-400">
+            <p class="region text-xl font-semibold capitalize flex gap-2 justify-start text-start p-2 mx-2 mt-4"><span class=" font-bold">Region:</span>${data[i].region}</p>
+            <p class="subregion text-xl font-semibold  capitalize flex gap-2 justify-start text-start p-2 mx-2"><span class=" font-bold">Subegion:</span> ${data[i].subregion}</p>
+            <p class="population text-xl font-semibold  capitalize flex gap-2 justify-start text-start p-2 mx-2 mb-4"><span class=" font-bold">Population:</span>${data[i].population}</p>
         </div>
     </div>`;
       document.body.append(div);
